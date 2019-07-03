fe8e6d2ad13af63256acee5ad99017f4d27ee83f
이기완 <kiwan.lee@catenoid.net>
2019-07-03 15:37:30 +0900

    Initial Commit

---
+++ VideoPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VideoPlayer
+//
+//  Created by kiwan on 03/07/2019.
+//  Copyright © 2019 kiwan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
